bc3b7d383953afd1d975b66541c5b0888b672305
Aidan Coyle <packrat386@gmail.com>
2023-08-09 10:10:14 -0500

    Add NWS weather alerts
    Closes #5
    Closes #7

--- lib/boreas/forecast_controller.rb
+++ lib/boreas/forecast_controller.rb
@@ -4,10 +4,20 @@ module Boreas
 
     def index
       @address = params.permit(:address).fetch(:address, "")
-      @forecast_data = ForecastService.new(@address).forecast_data unless @address.blank?
+
+      unless @address.blank?
+        @forecast_data = forecast_service.forecast_data
+        @alert_data = forecast_service.alert_data
+      end
     rescue => e
       Rails.logger.error [e.message, *e.backtrace].join($/)
       @error = e.message
+    end
+
+    private
+
+    def forecast_service
+      @forecast_service ||= ForecastService.new(@address)
     end
   end
 end
--- spec/boreas/forecast_service_spec.rb
+++ spec/boreas/forecast_service_spec.rb
@@ -6,97 +6,161 @@ require "spec_helper"
 # enough that I'm pretty confident it works anyway though.
 
 RSpec.describe Boreas::ForecastService do
-  let(:search_address) { "1600 Pennsylvania Avenue, Washington DC" }
   let(:service) { described_class.new(search_address) }
-  subject { service.forecast_data }
 
-  before do
-    stub_request(
-      :get,
-      "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=1600%20Pennsylvania%20Avenue,%20Washington%20DC&benchmark=2020&format=json"
-    ).to_return(
-      status: 200,
-      body: file_fixture("geocode_white_house.json")
-    )
+  describe "#forecast_data" do
+    let(:search_address) { "1600 Pennsylvania Avenue, Washington DC" }
+    subject { service.forecast_data }
 
-    stub_request(
-      :get,
-      "https://api.weather.gov/points/38.8988,-77.0353"
-    ).to_return(
-      status: 200,
-      body: file_fixture("nws_points_white_house.json")
-    )
-
-    stub_request(
-      :get,
-      "https://api.weather.gov/gridpoints/LWX/97,71/forecast"
-    ).to_return(
-      status: 200,
-      body: file_fixture("nws_forecast_white_house.json")
-    )
-
-    stub_request(
-      :get,
-      "https://api.weather.gov/gridpoints/LWX/97,71/forecast/hourly"
-    ).to_return(
-      status: 200,
-      body: file_fixture("nws_forecast_hourly_white_house.json")
-    )
-  end
-
-  let(:expected_format) { JSON.parse(file_fixture("white_house_expected_format.json").read) }
-
-  it "generates the expected data format" do
-    expect(subject).to eq(expected_format)
-  end
-
-  context "when there is no matching address" do
-    let(:search_address) { "notanaddress" }
-
-    before do
-      stub_request(
-        :get,
-        "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=notanaddress&benchmark=2020&format=json"
-      ).to_return(
-        status: 200,
-        body: file_fixture("geocode_no_match.json")
-      )
-    end
-
-    it "raises an error" do
-      expect { subject }.to raise_error(Boreas::ForecastService::NoMatchingAddressError)
-    end
-  end
-
-  context "when geocoding service errors" do
     before do
       stub_request(
         :get,
         "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=1600%20Pennsylvania%20Avenue,%20Washington%20DC&benchmark=2020&format=json"
       ).to_return(
-        status: 500,
-        body: {"ruh" => "roh"}.to_json
+        status: 200,
+        body: file_fixture("geocode_white_house.json")
       )
-    end
 
-    it "raises an error" do
-      expect { subject }.to raise_error(Boreas::ForecastService::APIError)
-    end
-  end
+      stub_request(
+        :get,
+        "https://api.weather.gov/points/38.8988,-77.0353"
+      ).to_return(
+        status: 200,
+        body: file_fixture("nws_points_white_house.json")
+      )
 
-  context "when forecast service errors" do
-    before do
       stub_request(
         :get,
         "https://api.weather.gov/gridpoints/LWX/97,71/forecast"
       ).to_return(
-        status: 500,
-        body: {"ruh" => "roh"}.to_json
+        status: 200,
+        body: file_fixture("nws_forecast_white_house.json")
+      )
+
+      stub_request(
+        :get,
+        "https://api.weather.gov/gridpoints/LWX/97,71/forecast/hourly"
+      ).to_return(
+        status: 200,
+        body: file_fixture("nws_forecast_hourly_white_house.json")
       )
     end
 
-    it "raises an error" do
-      expect { subject }.to raise_error(Boreas::ForecastService::APIError)
+    let(:expected_format) { JSON.parse(file_fixture("forecast_expected_format.json").read) }
+
+    it "generates the expected data format" do
+      expect(subject).to eq(expected_format)
+    end
+
+    context "when there is no matching address" do
+      let(:search_address) { "notanaddress" }
+
+      before do
+        stub_request(
+          :get,
+          "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=notanaddress&benchmark=2020&format=json"
+        ).to_return(
+          status: 200,
+          body: file_fixture("geocode_no_match.json")
+        )
+      end
+
+      it "raises an error" do
+        expect { subject }.to raise_error(Boreas::ForecastService::NoMatchingAddressError)
+      end
+    end
+
+    context "when geocoding service errors" do
+      before do
+        stub_request(
+          :get,
+          "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=1600%20Pennsylvania%20Avenue,%20Washington%20DC&benchmark=2020&format=json"
+        ).to_return(
+          status: 500,
+          body: {"ruh" => "roh"}.to_json
+        )
+      end
+
+      it "raises an error" do
+        expect { subject }.to raise_error(Boreas::ForecastService::APIError)
+      end
+    end
+
+    context "when forecast service errors" do
+      before do
+        stub_request(
+          :get,
+          "https://api.weather.gov/gridpoints/LWX/97,71/forecast"
+        ).to_return(
+          status: 500,
+          body: {"ruh" => "roh"}.to_json
+        )
+      end
+
+      it "raises an error" do
+        expect { subject }.to raise_error(Boreas::ForecastService::APIError)
+      end
+    end
+  end
+
+  describe "#alert_data" do
+    let(:search_address) { "1107 Douglas Ave, Beaver, OK 73932" }
+    subject { service.alert_data }
+
+    before do
+      stub_request(
+        :get,
+        "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=1107+Douglas+Ave%2C+Beaver%2C+OK+73932&benchmark=2020&format=json"
+      ).to_return(
+        status: 200,
+        body: file_fixture("geocode_beaver_county_fair.json")
+      )
+
+      stub_request(
+        :get,
+        "https://api.weather.gov/alerts/active?point=36.806%2C-100.5196"
+      ).to_return(
+        status: 200,
+        body: file_fixture("nws_alerts_beaver_county_fair.json")
+      )
+    end
+
+    let(:expected_format) { JSON.parse(file_fixture("alert_expected_format.json").read) }
+
+    it "generates the expected data format" do
+      expect(subject).to eq(expected_format)
+    end
+
+    context "when geocoding service errors" do
+      before do
+        stub_request(
+          :get,
+          "https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=1107+Douglas+Ave%2C+Beaver%2C+OK+73932&benchmark=2020&format=json"
+        ).to_return(
+          status: 500,
+          body: {"ruh" => "roh"}.to_json
+        )
+      end
+
+      it "raises an error" do
+        expect { subject }.to raise_error(Boreas::ForecastService::APIError)
+      end
+    end
+
+    context "when forecast service errors" do
+      before do
+        stub_request(
+          :get,
+          "https://api.weather.gov/alerts/active?point=36.806%2C-100.5196"
+        ).to_return(
+          status: 500,
+          body: {"ruh" => "roh"}.to_json
+        )
+      end
+
+      it "raises an error" do
+        expect { subject }.to raise_error(Boreas::ForecastService::APIError)
+      end
     end
   end
 end
