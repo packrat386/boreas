d34d225b58f605adb8a55dd99c876e63fd954936
Aidan Coyle <packrat386@gmail.com>
2026-04-07 09:53:27 -0500

    Version 1.1.1

--- lib/boreas/application.rb
+++ lib/boreas/application.rb
@@ -1,7 +1,7 @@
 require "action_controller/railtie"
 
 module Boreas
-  VERSION = "1.1.0"
+  VERSION = "1.1.1"
 
   class Application < Rails::Application
     config.logger = Logger.new(Rails.env.test? ? nil : $stdout)
